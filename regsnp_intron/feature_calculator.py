d53e8b0df1812af7013bbd3f22f5633bbe1832e6
linhai <linhai@iupui.edu>
2016-07-29 13:04:47 -0400

    add function to check the input format.

--- regsnp_intron/feature_calculator.py
+++ regsnp_intron/feature_calculator.py
@@ -29,35 +29,44 @@ class FeatureCalculator(object):
         out_dir_tmp = os.path.join(self.out_dir, 'tmp')
         os.mkdir(out_dir_tmp)
 
-        # sort input
-        self.logger.info('Sorting input file.')
+        # Check input format
+        self.logger.info('Checking input file format.')
         snp = SNP(self.ifname)
+        ref_name = os.path.join(self.db_dir, 'hg19/hg19.fa')
+        if not snp.is_valid(ref_name):
+            raise RuntimeError('Invalid input file format.')
+
+        # Sort input
+        self.logger.info('Sorting input file.')
         snp.sort(os.path.join(out_dir_tmp, 'snp.sorted'))
 
-        # annotate SNVs
+        # Annotate SNVs
         self.logger.info('Annotating SNVs with ANNOVAR.')
         annovar_path = os.path.expanduser(self.settings['annovar_path'])
         annovar_db_path = os.path.expanduser(self.settings['annovar_db_path'])
         annovar = Annovar(annovar_path, annovar_db_path)
         annovar.annotate(os.path.join(out_dir_tmp, 'snp.sorted'), os.path.join(out_dir_tmp, 'snp'))
 
-        # calculate distance to closest protein coding exons and extract intronic SNVs
+        # Calculate distance to closest protein coding exons and extract intronic SNVs
         self.logger.info('Calculating distance to closest protein coding exons.')
         protein_coding_exon_fname = os.path.join(self.db_dir, 'hg19_ensGene_exon.bed')
         closest_exon = ClosestExon(protein_coding_exon_fname)
         closest_exon.get_closest_exon(os.path.join(out_dir_tmp, 'snp.intronic'),
                                       os.path.join(out_dir_tmp, 'snp.distance'))
 
-        # extract flanking sequence
+        if os.path.getsize(os.path.join(out_dir_tmp, 'snp.distance')) <= 0:
+            self.logger.error('No input SNVs are in valid intronic regions.')
+            raise RuntimeError('No input SNVs are in valid intronic regions.')
+
+        # Extract flanking sequence
         self.logger.info('Fetching flanking sequence.')
-        ref_name = os.path.join(self.db_dir, 'hg19/hg19.fa')
         seq = FlankingSeq(ref_name, 20)
         seq.fetch_flanking_seq(os.path.join(out_dir_tmp, 'snp.distance'), os.path.join(out_dir_tmp, 'snp.seq'))
         seq.fetch_flanking_seq(os.path.join(out_dir_tmp, 'snp.distance'), os.path.join(out_dir_tmp, 'snp.fa'),
                                otype='fasta')
         seq.close()
 
-        # calculate RBP binding change
+        # Calculate RBP binding change
         self.logger.info('Calculating RBP binding change.')
         pssm_path = os.path.join(self.db_dir, 'motif/pwm')
         pssm_list_fname = os.path.join(self.db_dir, 'motif/pwm_valid.txt')
@@ -67,7 +76,7 @@ class FeatureCalculator(object):
                                     os.path.join(out_dir_tmp, 'snp.rbp_score'))
         rbp.cal_change(os.path.join(out_dir_tmp, 'snp.rbp_score'), os.path.join(out_dir_tmp, 'snp.rbp_change'))
 
-        # calculate protein structural features (disorder, secondary structure, ASA, Pfam, PTM)
+        # Calculate protein structural features (disorder, secondary structure, ASA, Pfam, PTM)
         self.logger.info('Extracting protein structural features.')
         db_fname = os.path.join(self.db_dir, 'ensembl.db')
         gene_pred_fname = os.path.join(self.db_dir, 'hg19_ensGene.txt')
@@ -75,7 +84,7 @@ class FeatureCalculator(object):
         protein_feature.calculate_protein_feature(os.path.join(out_dir_tmp, 'snp.distance'),
                                                   os.path.join(out_dir_tmp, 'snp.protein_feature'))
 
-        # calculate junction score
+        # Calculate junction score
         self.logger.info('Calculating junction strength change.')
         donor_ic_fname = os.path.join(self.db_dir, 'motif/donorsite.pssm')
         acceptor_ic_fname = os.path.join(self.db_dir, 'motif/acceptorsite.pssm')
@@ -83,7 +92,7 @@ class FeatureCalculator(object):
         junction_strength.cal_junction_strength(ref_name, os.path.join(out_dir_tmp, 'snp.distance'),
                                                 os.path.join(out_dir_tmp, 'snp.junc'))
 
-        # calculate phylop conservation score
+        # Calculate phylop conservation score
         self.logger.info('Calculating conservation score.')
         phylop_fname = os.path.join(self.db_dir, 'phylop/hg19.100way.phyloP100way.bw')
         phylop = Phylop(phylop_fname)
--- regsnp_intron/utils/snp.py
+++ regsnp_intron/utils/snp.py
@@ -1,17 +1,66 @@
 #!/usr/bin/env python
 import argparse
+import logging
 import os.path
 
 import pandas as pd
-
+import pysam
 
 class SNP(object):
     def __init__(self, in_fname):
         self.in_fname = os.path.expanduser(in_fname)
+        self.logger = logging.getLogger(__name__)
 
     def sort(self, out_fname):
         in_f = pd.read_csv(self.in_fname, sep='\s+', names=['#chrom', 'pos', 'ref', 'alt'])
         in_f.sort_values(by=['#chrom', 'pos']).to_csv(out_fname, sep='\t', header=False, index=False)
+
+    def is_valid(self, ref_fname):
+        """
+        An SNV input file is valid if:
+            1. contains four columns: chrom, pos, ref, alt
+            2. chrom and pos are in reference genome.
+            3. ref and alt are single nucleotide allele
+        :param ref_fname: reference fasta file with .fai index.
+        :return: a boolean value.
+        """
+        if os.path.getsize(self.in_fname) <= 0:
+            self.logger.error('Input file is empty.')
+            return False
+        with open(self.in_fname) as f, pysam.FastaFile(ref_fname) as ref:
+            references = dict(zip(ref.references, ref.lengths))
+            alleles = {'A', 'C', 'G', 'T'}
+            for line in f:
+                cols = line.rstrip().split()
+                if len(cols) != 4:
+                    self.logger.error('Invalid file format: {0}. '
+                                      'File should contain tab-delimited four columns: chrom, pos, ref, alt'.
+                                      format(line.rstrip()))
+                    return False
+                chrom, pos, ref_allele, alt_allele = cols
+                if chrom not in references:
+                    self.logger.error('Chromosome cannot be found in the reference: {0}.'.format(line.rstrip()))
+                    return False
+                if ref_allele not in alleles or alt_allele not in alleles:
+                    self.logger.error('Ref and alt should be in {{A, C, G, T}}: {0}'.format(line.rstrip()))
+                    #self.logger.error('Ref and alt should be in {A, C, G, T}: {0}.'.format(line.rstrip()))
+                    return False
+                try:
+                    pos = int(pos)
+                except ValueError:
+                    self.logger.error('Coordinate must be integer: {0}.'.format(line.rstrip()))
+                    return False
+                if pos > references[chrom] or pos < 0:
+                    self.logger.error('Coordinate exceed the range of chromosome: {0}.'.
+                                      format(line.rstrip()))
+                    return False
+                if ref_allele != ref.fetch(reference=chrom, start=(pos - 1), end=pos).upper():
+                    self.logger.error('Input reference allele is not consistent with reference genome: {0}.'.
+                                      format(line.rstrip()))
+                    return False
+        return True
+
+
 
 
 def main():
